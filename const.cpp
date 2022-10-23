35324113f75aa13ca8439d1f18094b81a5aa5e02
Fahama Khan <fahamakhan990@gmail.com>
2022-10-23 23:13:39 +0500

    add const file

---
+++ const.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+	
+	//const specifies that a vars' value is constant
+	
+	const double pi = 3.14;
+	double radius = 10;
+	double circumference = 2 * pi * radius;
+	
+	std::cout << circumference << " cm";
+	
+	
+	return 0;
+}
